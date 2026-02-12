e9a54c6747af6e55243410652e244e3118337e24
DevvGames <102315175+DevvGamesRblx@users.noreply.github.com>
2026-02-12 14:53:44 +0200

    Add files via upload

---
+++ data/atms&vaults.js
@@ -0,0 +1,19 @@
+const ATMS_DATA = [
+  { name: "Green ATM", rarity: "COMMON", price: 200 },
+  { name: "Blue ATM", rarity: "COMMON", price: 300 },
+  { name: "Purple ATM", rarity: "UNCOMMON", price: 500 },
+  { name: "Red ATM", rarity: "EPIC", price: 750 },
+  { name: "Gold ATM", rarity: "MYTHIC", price: 10000 },
+  { name: "Diamond ATM", rarity: "LEGENDARY", price: 100000 },
+  { name: "Void ATM", rarity: "LEGENDARY", price: 500000 }
+];
+
+const VAULTS_DATA = [
+  { name: "Regular Vault", rarity: "COMMON", price: 500 },
+  { name: "Silver Vault", rarity: "COMMON", price: 1000 },
+  { name: "Gold Vault", rarity: "UNCOMMON", price: 2500 },
+  { name: "Sapphire Vault", rarity: "EPIC", price: 5000 },
+  { name: "Ruby Vault", rarity: "LEGENDARY", price: 15000 },
+  { name: "Emerald Vault", rarity: "LEGENDARY", price: 50000 },
+  { name: "Amethyst Vault", rarity: "MYTHIC", price: 100000 }
+];

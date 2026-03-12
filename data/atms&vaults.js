31205507402eb2d3df54a22fa33085836b1c1e4e
web-davy <the-davy-devv@gmail.com>
2026-03-12 13:57:23 +0200

    Stuff

--- data/atms&vaults.js
+++ data/atms&vaults.js
@@ -1,11 +1,11 @@
 const ATMS_DATA = [
-  { name: "Green ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 1.86", price: 200 },
-  { name: "Blue ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 3.76", price: 300 },
-  { name: "Purple ATM", showMoreButton: false, rarity: "UNCOMMON", rarityPercent: "1 in 6.00", price: 500 },
-  { name: "Red ATM", showMoreButton: false, rarity: "EPIC", rarityPercent: "1 in 47.46", price: 650 },
-  { name: "Gold ATM", showMoreButton: false, rarity: "MYTHIC", rarityPercent: "1 in 230.50", price: 10000 },
-  { name: "Diamond ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 403.38", price: 100000 },
-  { name: "Void ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 1613.50", price: 500000 }
+  { name: "Green ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 1.86 [unchecked]", price: 200 },
+  { name: "Blue ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 3.76 [unchecked]", price: 300 },
+  { name: "Purple ATM", showMoreButton: false, rarity: "UNCOMMON", rarityPercent: "1 in 6.00 [unchecked]", price: 500 },
+  { name: "Red ATM", showMoreButton: false, rarity: "EPIC", rarityPercent: "1 in 47.46 [unchecked]", price: 650 },
+  { name: "Gold ATM", showMoreButton: false, rarity: "MYTHIC", rarityPercent: "1 in 230.50 [unchecked]", price: 10000 },
+  { name: "Diamond ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 403.38 [unchecked]", price: 100000 },
+  { name: "Void ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 1613.50 (unchecked)", price: 500000 }
 ];
 
 const VAULTS_DATA = [

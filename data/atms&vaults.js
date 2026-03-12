c93308c6b35ecedf70c5e312b564565f9454bf8b
web-davy <the-davy-devv@gmail.com>
2026-03-12 13:44:35 +0200

    Changed rarity

--- data/atms&vaults.js
+++ data/atms&vaults.js
@@ -1,19 +1,19 @@
 const ATMS_DATA = [
-  { name: "Green ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "53.89%", price: 200 },
-  { name: "Blue ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "26.94%", price: 300 },
-  { name: "Purple ATM", showMoreButton: false, rarity: "UNCOMMON", rarityPercent: "16.16%", price: 500 },
-  { name: "Red ATM", showMoreButton: false, rarity: "EPIC", rarityPercent: "2.16%", price: 650 },
-  { name: "Gold ATM", showMoreButton: false, rarity: "MYTHIC", rarityPercent: "0.539%", price: 10000 },
-  { name: "Diamond ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "0.269%", price: 100000 },
-  { name: "Void ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "0.027%", price: 500000 }
+  { name: "Green ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 1.86", price: 200 },
+  { name: "Blue ATM", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 3.76", price: 300 },
+  { name: "Purple ATM", showMoreButton: false, rarity: "UNCOMMON", rarityPercent: "1 in 6.00", price: 500 },
+  { name: "Red ATM", showMoreButton: false, rarity: "EPIC", rarityPercent: "1 in 47.46", price: 650 },
+  { name: "Gold ATM", showMoreButton: false, rarity: "MYTHIC", rarityPercent: "1 in 230.50", price: 10000 },
+  { name: "Diamond ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 403.38", price: 100000 },
+  { name: "Void ATM", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 1613.50", price: 500000 }
 ];
 
 const VAULTS_DATA = [
-  { name: "Regular Vault", showMoreButton: false, rarity: "COMMON", rarityPercent: "53.89%", priceMin: 5000, priceMax: 7500 },
-  { name: "Silver Vault", showMoreButton: false, rarity: "COMMON", rarityPercent: "26.94%", priceMin: 7500, priceMax: 10000 },
-  { name: "Gold Vault", showMoreButton: false, rarity: "UNCOMMON", rarityPercent: "16.16%", priceMin: 15000, priceMax: 25000 },
-  { name: "Sapphire Vault", showMoreButton: false, rarity: "EPIC", rarityPercent: "2.16%", priceMin: 40000, priceMax: 50000 },
-  { name: "Ruby Vault", showMoreButton: false, rarity: "MYTHIC", rarityPercent: "0.539%", priceMin: 80000, priceMax: 90000 },
-  { name: "Emerald Vault", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "0.269%", priceMin: 125000, priceMax: 225000 },
-  { name: "Amethyst Vault", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "0.027%", priceMin: 225000, priceMax: 350000 }
+  { name: "Regular Vault", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 1.86", priceMin: 5000, priceMax: 7500 },
+  { name: "Silver Vault", showMoreButton: false, rarity: "COMMON", rarityPercent: "1 in 3.76", priceMin: 7500, priceMax: 10000 },
+  { name: "Gold Vault", showMoreButton: false, rarity: "UNCOMMON", rarityPercent: "1 in 6.00", priceMin: 15000, priceMax: 25000 },
+  { name: "Sapphire Vault", showMoreButton: false, rarity: "EPIC", rarityPercent: "1 in 47.46", priceMin: 40000, priceMax: 50000 },
+  { name: "Ruby Vault", showMoreButton: false, rarity: "MYTHIC", rarityPercent: "1 in 230.50", priceMin: 80000, priceMax: 90000 },
+  { name: "Emerald Vault", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 403.38", priceMin: 125000, priceMax: 225000 },
+  { name: "Amethyst Vault", showMoreButton: false, rarity: "LEGENDARY", rarityPercent: "1 in 1613.50", priceMin: 225000, priceMax: 350000 }
 ];

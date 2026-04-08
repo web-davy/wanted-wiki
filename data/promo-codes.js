c9df2d3d64f4202476d46cab9b726bfc84f15791
web-davy <web-davy@users.noreply.github.com>
2026-04-08 12:38:52 +0300

    updated zorvello promocode to be expired

--- data/promo-codes.js
+++ data/promo-codes.js
@@ -4,7 +4,7 @@ const PROMO_CODES_DATA = [
     { code: "WANTED", reward: "$10,000 Cash", description: "Free 10k cash", active: true },
     { code: "BOOM", reward: "10 C4", description: "Free 10 C4", active: true },
     { code: "SANTA", reward: "$10,000 Cash", description: "Free 10k cash", active: false },
-    { code: "ZORVELLO", reward: "Zorvello Chair Funiture", description: "Free Zorvello Chair Funiture", active: true },
+    { code: "ZORVELLO", reward: "Zorvello Chair Funiture", description: "Free Zorvello Chair Funiture", active: false },
     { code: "FRAG", reward: "Frag Grenade", description: "Free M67 Frag Grenade", active: true },
     { code: "EASTER", reward: "Egg Display Furniture", description: "Free Egg Display Furniture", active: true }
 ];

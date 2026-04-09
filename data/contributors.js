4a576842f22acacea581ad4a474106fb6dc2d22a
web-davy <web-davy@users.noreply.github.com>
2026-04-09 10:14:56 +0300

    new staff

--- data/contributors.js
+++ data/contributors.js
@@ -5,12 +5,13 @@ const CONTRIBUTORS_DATA = {
     staff: [
         { name: "MeepMeep", title: "Wiki Staff" },
         { name: "Emperor", title: "Wiki Staff" },
-        { name: "Professor Proish", title: "Wiki Staff" }
+        { name: "Professor Proish", title: "Wiki Staff" },
+        { name: "xTrench", title: "Wiki Staff" }
     ],
     contributors: [
         { name: "Justin", contributions: "In-Game Cash" },
         { name: "RedRuby", contributions: "Vault Images" },
-        { name: "Baboo", contributions: "Deagle and FN-FAL Images" },
+        { name: "Borgaboo", contributions: "Deagle and FN-FAL Images" },
         { name: "Ravozir", contributions: "Blade Info" },
         { name: "VibinLaz3r", contributions: "Rod Loot" },
         { name: "Ravozir", contributions: "Rod Loot" },
